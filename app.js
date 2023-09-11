f65017e46ce663f4b7c3d13d15b6d7745a6bcae8
Deepak.malik-at-527579388740 <Deepak.malik-at-527579388740>
2023-09-11 10:15:58 +0530

    security packages added

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const authRouter = require("./routes/authRouter");
 const errorHandler = require("./middlewares/errorandler");
 const authMiddleware = require("./middlewares/authMiddleware");
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const connectDB = require("./db/connect");
 
 const app = express();
@@ -15,7 +20,18 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 mins
+    max: 50,
+  })
+);
 app.use(express.json());
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 app.use("/api/v1/auth", authRouter);
